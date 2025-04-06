28ab7805fac3a737629c1007c2930675bb43d5d8
henryhazelton <henryhazelton00@gmail.com>
2025-04-06 16:53:19 +0100

    added the player.py file and managed to get the triangle player icon to show

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame # type: ignore
 from constants import *
+from player import *
 
 def main():
     
@@ -12,12 +13,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x, y)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000 # Limits the FPS to 60
