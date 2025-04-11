526123b2d3f1cb85a0ea58a61d573fd0eac30116
henryhazelton <henryhazelton00@gmail.com>
2025-04-11 16:47:03 +0100

    edited the group names and loop variable name

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    group_updatable = pygame.sprite.Group()
-    group_drawable = pygame.sprite.Group()
-    Player.containers = (group_updatable, group_drawable)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
 
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
@@ -27,11 +27,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        group_updatable.update(dt) 
+        updatable.update(dt) 
 
         screen.fill("black")
-        for thing in group_drawable: 
-            thing.draw(screen)
+        for obj in drawable: 
+            obj.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000 # Limits the FPS to 60
