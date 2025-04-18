0a4df8c5052c3d1b6bc7108662ce640d532bf202
henryhazelton <henryhazelton00@gmail.com>
2025-04-18 16:23:35 +0100

    added the ability to shoot but added a delay so the weapon is not over powered

--- player.py
+++ player.py
@@ -7,7 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-    
+        self.timer = 0
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -25,6 +25,11 @@ class Player(CircleShape):
         return self.rotation 
     
     def update(self, dt):
+        if self.timer > 0:
+            self.timer -= dt
+        if self.timer < 0:
+            self.timer = 0
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -40,7 +45,10 @@ class Player(CircleShape):
             self.move(-dt)
 
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.timer <= 0:
+                self.shoot()
+                self.timer = PLAYER_SHOOT_COOLDOWN
+                
         
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -50,5 +58,7 @@ class Player(CircleShape):
     def shoot(self):
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOT_SPEED # The bullet that will hit the asteroid coming from the direction the player is facing
+        
+        
 
         
