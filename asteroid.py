43fa860663cb1276e67039aac7bdae9bbf0ad7df
henryhazelton <henryhazelton00@gmail.com>
2025-12-19 16:48:45 +0000

    ran ruff format

--- asteroid.py
+++ asteroid.py
@@ -3,28 +3,31 @@ import random
 from circleshape import CircleShape
 from constants import ASTEROID_MIN_RADIUS
 
+
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
-        
-       
+
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", center=self.position, radius=self.radius, width=2) # Hopefully this draws a circle correctly
-        
+        pygame.draw.circle(
+            screen, "white", center=self.position, radius=self.radius, width=2
+        )  # Hopefully this draws a circle correctly
 
     # Should update the display so that the asteroid travels in a straight line
     def update(self, dt):
         self.position += self.velocity * dt
-        
+
     def split(self):
         # Firstly, should kill the asteroid and then split
         self.kill()
         if self.radius <= ASTEROID_MIN_RADIUS:
             return
         old_radius = self.radius
-        random_angle = random.uniform(20, 50) # Create a random angle between 20 & 50 degrees that the new asteroids will split at
+        random_angle = random.uniform(
+            20, 50
+        )  # Create a random angle between 20 & 50 degrees that the new asteroids will split at
 
-        # The new velocity vectors for the asteroids 
+        # The new velocity vectors for the asteroids
         asteroid_1_velocity_vector = self.velocity.rotate(random_angle)
         asteroid_2_velocity_vector = self.velocity.rotate(-random_angle)
 
@@ -32,10 +35,13 @@ class Asteroid(CircleShape):
         new_radius_of_asteroid = old_radius - ASTEROID_MIN_RADIUS
 
         # Create the two new asteroid objects
-        asteroid_1 = Asteroid(self.position.x, self.position.y, radius=new_radius_of_asteroid)
-        asteroid_2 = Asteroid(self.position.x, self.position.y, radius=new_radius_of_asteroid)
+        asteroid_1 = Asteroid(
+            self.position.x, self.position.y, radius=new_radius_of_asteroid
+        )
+        asteroid_2 = Asteroid(
+            self.position.x, self.position.y, radius=new_radius_of_asteroid
+        )
 
         # Setting the new asteroids velocities
         asteroid_1.velocity = asteroid_1_velocity_vector * 1.2
         asteroid_2.velocity = asteroid_2_velocity_vector * 1.4
-        
--- main.py
+++ main.py
@@ -2,21 +2,20 @@
 # the open-source pygame library
 # throughout this file
 import sys
-import pygame # type: ignore
+import pygame  # type: ignore
 from constants import *
 from player import *
 from asteroid import *
 from asteroidfield import *
 
+
 def main():
-    
     # Initalisation code
     print("Starting Asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    
 
     # Making containers to hold the objects
     updatable = pygame.sprite.Group()
@@ -33,35 +32,34 @@ def main():
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
 
-
     player = Player(x, y)
     asteroid_field = AsteroidField()
 
-    # The Game Loop 
+    # The Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        updatable.update(dt) 
+
+        updatable.update(dt)
 
         for asteriod in asteroids:
             if asteriod.collides_with(player):
                 print("Game over!")
                 sys.exit()
-            
+
             for shot in shots:
                 if asteriod.collides_with(shot):
                     shot.kill()
                     asteriod.split()
 
         screen.fill("black")
-        for obj in drawable: 
+        for obj in drawable:
             obj.draw(screen)
         pygame.display.flip()
 
-        dt = clock.tick(60) / 1000 # Limits the FPS to 60
-        
+        dt = clock.tick(60) / 1000  # Limits the FPS to 60
+
 
 if __name__ == "__main__":
     main()
