07caebf471281275b8b90713a24d2322c3012f37
henryhazelton <henryhazelton00@gmail.com>
2025-12-23 15:52:02 +0000

    added screen height/width display to user

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ from asteroidfield import AsteroidField
 def main():
     # Initalisation code
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
